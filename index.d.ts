5727445e451a9b70689fc36e9a42f8ba4842ec44
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-12-28 16:58:40 +0800

    added method model() in Horizon instance

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 // Source: https://raw.githubusercontent.com/beenotung/typed-horizon/master/src/main.d.ts
-import * as Rx from 'rxjs';
+import {Observable} from "rxjs";
+
 export interface Horizon {
-  find<A>(): Rx.Observable<A>;
+  find<A>(): Observable<A>;
 
   call<A>(_this: Horizon, table: string): TableObject<A>;
 
@@ -18,6 +19,8 @@ export interface Horizon {
   onDisconnected(f: Function): void;
 
   onSocketError(f: (error: any) => void): void;
+
+  model<A>(query: (id: string) => AggregateObject): (id: string) => FinalQuery<A>;
 }
 export interface HorizonConstructorParam {
   host?: string;        // default to window.location
@@ -30,7 +33,7 @@ export interface HorizonConstructor {
   new(param?: HorizonConstructorParam): Horizon;
   clearAuthToken(): void;
 }
-export var Horizon: HorizonConstructor;
+export let Horizon: HorizonConstructor;
 
 export type AuthType  = 'unauthenticated' | 'anonymous' | 'token';
 export type AuthToken = {token: any, storeLocally: boolean};
@@ -41,15 +44,15 @@ export interface TableQuery<A> extends FinalQuery<A> {
   order(field: string, direction?: OrderType): OrderQuery<A>;     // default to "ascending"
   above(idOrObject: string|any, type?: RangeType): OrderQuery<A>; // default to "open" (exclusive)
 }
-export interface FinalQuery<A> {
+export interface FinalQuery<A> extends LimitedFinalQuery<A> {
   limit(max: number): LimitedFinalQuery<A>;
-  fetch(): Rx.Observable<A[]>;
 }
-export interface SingleFinalQuery<A> extends Rx.Observable<A> {
-  defaultIfEmpty(): Rx.Observable<A>;
+export interface SingleFinalQuery<A> extends Observable<A> {
+  defaultIfEmpty(): Observable<A>;
 }
 export interface LimitedFinalQuery<A> {
-  fetch(): Rx.Observable<A[]>;
+  fetch(): Observable<A[]>;
+  watch(): Observable<A[]>;
 }
 export interface OrderQuery<A> extends FinalQuery<A> {
   below(idOrObject: string|any, type?: string): FinalQuery<A[]>; // default open(exclusive)
@@ -60,10 +63,15 @@ export interface FindQuery<A> {
 export interface CreatedObject {
   id: string;
 }
+export interface JSONObject {
+  [key: string]: DataType|JSONObject|Array<DataType>;
+}
+export type DataType = number|string|boolean|null|JSONObject
+  |number[]|string[]|boolean[]|null[]|JSONObject[];
 /* local record, not yet stored */
 export interface NewRecord {
   id?: string;
-  [key: string]: string|Array<NewRecord>|number|boolean|NewRecord;
+  [key: string]: DataType;
 }
 /* get from database */
 export interface OldRecord extends NewRecord {
@@ -74,9 +82,12 @@ export interface TableObject<A> extends TableQuery<A> {
   findAll(x: string|NewRecord, ...xs: Array<string|NewRecord>): TableQuery<A>;
   insert(oneOrList: NewRecord|NewRecord[]): TableObject<CreatedObject>;
   remove(x: string|OldRecord): TableObject<CreatedObject>;
-  removeAll(xs: Array<string|OldRecord>): Rx.Observable<CreatedObject>;
-  replace(oneOrList: OldRecord|OldRecord[]): Rx.Observable<CreatedObject>;
-  store(oneOrList: NewRecord|OldRecord|(NewRecord|OldRecord)[]): Rx.Observable<CreatedObject>;
+  removeAll(xs: Array<string|OldRecord>): Observable<CreatedObject>;
+  replace(oneOrList: OldRecord|OldRecord[]): Observable<CreatedObject>;
+  store(oneOrList: NewRecord|OldRecord|(NewRecord|OldRecord)[]): Observable<CreatedObject>;
   update(oneOrList: OldRecord|OldRecord[]): TableQuery<CreatedObject>;
   upsert(oneOrList: NewRecord|OldRecord|(NewRecord|OldRecord)[]): TableQuery<CreatedObject>;
+}
+export interface AggregateObject {
+  [key: string]: FindQuery<NewRecord>|DataType|Observable<NewRecord>|Promise<NewRecord>|AggregateObject[]
 }

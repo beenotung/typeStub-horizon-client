4a5980f0acbca3035340688097b79fd859524302
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-09-29 16:08:51 +0800

    provide impl from @horizon/client

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+// if the user want to use other version, e.g. from server,
 // either load from <src> or fetch from server, then resolve from global var Horizon;
 // or import from '@horizon/client'.
+// Example:
+// import _Horizon_ = require('@horizon/client');
+// import { Horizon, HorizonConstructor } from 'typestub-horizon-client';
+// export let _Horizon: HorizonConstructor = _Horizon_;
+
+exports.Horizon = require('@horizon/client');
